361f28abe12fc1acbb895abbc9bd7720c191a615
linda-20 <lindaaron40@gmail.com>
2024-03-05 14:22:38 +0300

    Completed assignment

--- lib/user.py
+++ lib/user.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
 
 class User:
+    def __init__(self,first_namename,last_namename):
+        self.first_name = first_namename
+        self.last_name = last_namename
     pass
